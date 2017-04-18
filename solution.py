641cd66ed2d868c550a6b878bfbaf69f43fb93c7
Mateusz Dziubek <mateusz.dziubek@schibsted.pl>
2017-04-18 11:19:00 +0200

    Implemented reduce_puzzle

--- solution.py
+++ solution.py
@@ -135,7 +135,16 @@ def only_choice(values):
 
 
 def reduce_puzzle(values):
-    pass
+    stalled = False
+    while not stalled:
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+        values = eliminate(values)
+        values = only_choice(values)
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        stalled = solved_values_before == solved_values_after
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
 
 
 def search(values):
